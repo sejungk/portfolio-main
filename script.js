c259860e048e0f0e7cd06b0c8482bc876187dff1
Sejung <49921709+sejungk@users.noreply.github.com>
2023-08-08 21:47:15 -0400

    added js color hover animation

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const projectLinks = document.querySelectorAll('.main__project-links');
+  const linkColors = [
+    '#ADE3F0', '#F6C0C0', '#C2B6E5', '#E3B8E3', '#B2BBDC', '#EFE7BB', '#EDBD9A'
+  ];
+
+  projectLinks.forEach((link, index) => {
+    link.addEventListener('mouseover', () => {
+      link.style.backgroundColor = linkColors[index];
+    });
+
+    link.addEventListener('mouseout', () => {
+      link.style.backgroundColor = ''; // Reset to default background
+    });
+  });
+});
